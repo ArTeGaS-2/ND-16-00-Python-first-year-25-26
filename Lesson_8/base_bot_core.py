2e17e28e84fc6d5ae1f7093aa76c2077b701d0bc
ArTeGaS-2 <probablytorvik@gmail.com>
2025-11-16 20:02:32 +0200

    Зробили базовий шаблон телеграм бота, та почали розробку бота з підключенням до OpenAI ChatGPT

---
+++ Lesson_8/base_bot_core.py
@@ -0,0 +1,21 @@
+# pip install python-telegram-bot
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+TOKEN = "8007498060:AAEsmpsDhAACsZKlOBkGl8ltZG5Nhg8StRs"
+
+async def start(update, context):
+    await update.message.reply_text("Привіт! Я твій новий бот.")
+
+async def echo(update, context):
+    await update.message.reply_text(update.message.text)
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT, echo))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
